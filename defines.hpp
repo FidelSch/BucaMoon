49ed70e383814f2b4e76b3b1b642caaee1f7132c
FidelSch <honoratofidel@protonmail.com>
2024-10-15 11:41:59 -0300

    Better UUID documentation

--- defines.hpp
+++ defines.hpp
@@ -1,6 +1,9 @@
 #pragma once
 
-#define SERVICE_UUID           "6E400001-B5A3-F393-E0A9-E50E24DCCA9E" // UART service UUID
+// Nordic UART Service UUIDs
+// Used for the app to recognize the device as a valid moonboard
+// See more at https://developer.nordicsemi.com/nRF_Connect_SDK/doc/v1.7-branch/nrf/libraries/bluetooth_services/services/nus.html
+#define SERVICE_UUID           "6E400001-B5A3-F393-E0A9-E50E24DCCA9E" 
 #define CHARACTERISTIC_UUID_RX "6E400002-B5A3-F393-E0A9-E50E24DCCA9E"
 #define CHARACTERISTIC_UUID_TX "6E400003-B5A3-F393-E0A9-E50E24DCCA9E"
 
