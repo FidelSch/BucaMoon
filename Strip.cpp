b49aa580208b44c8d209e55f9e49464ba148a25a
FidelSch <honoratofidel@protonmail.com>
2024-11-15 09:25:41 -0300

    Beta colors

--- Strip.cpp
+++ Strip.cpp
@@ -2,10 +2,13 @@
 #include "Hold.hpp"
 #include <Adafruit_NeoPixel.h>
 
-constexpr uint32_t RGB_RED = 0xFF0000;
-constexpr uint32_t RGB_GREEN = 0x00FF00;
-constexpr uint32_t RGB_BLUE = 0x0000FF;
-constexpr uint32_t RGB_BLACK = 0x000000;
+constexpr uint32_t RGB_RED       = 0xFF0000;
+constexpr uint32_t RGB_GREEN     = 0x00FF00;
+constexpr uint32_t RGB_BLUE      = 0x0000FF;
+constexpr uint32_t RGB_BLACK     = 0x000000;
+constexpr uint32_t RGB_LIGHTBLUE = 0x48C2FF;
+constexpr uint32_t RGB_VIOLET    = 0xAA48FF;
+constexpr uint32_t RGB_PINK      = 0xFF7BAB;
 
 static Adafruit_NeoPixel strip(HOLD_AMOUNT, OUTPUT_PIN, NEO_RGB | NEO_KHZ800);
 
@@ -38,8 +41,28 @@ void runInitAnimation(void)
 
 /// @brief Translates hold buffer into board
 /// @param holds Hold buffer
+/// @param showMoveBeta 
 void showBoard(const std::array<Hold::HOLDTYPE_t, HOLD_AMOUNT> holds, const bool showMoveBeta)
 {
+      const uint32_t START_COLOR      = RGB_GREEN;
+      const uint32_t RIGHT_COLOR      = RGB_BLUE;
+      const uint32_t PROGRESS_COLOR   = RGB_BLUE;
+      const uint32_t ADDITIONAL_COLOR = RGB_BLUE;
+      const uint32_t END_COLOR        = RGB_RED;
+      const uint32_t NO_COLOR         = RGB_BLACK;
+
+      // Optional beta colors
+      uint32_t LEFT_COLOR       = RGB_BLUE;
+      uint32_t FOOT_COLOR       = RGB_BLUE;
+      uint32_t MATCH_COLOR      = RGB_BLUE;
+
+      if (showMoveBeta)
+      {
+            LEFT_COLOR = RGB_VIOLET;
+            FOOT_COLOR = RGB_LIGHTBLUE;
+            MATCH_COLOR = RGB_PINK;
+      }
+
       strip.begin();
 
       for (size_t i = 0; i < HOLD_AMOUNT; i++)
@@ -47,22 +70,32 @@ void showBoard(const std::array<Hold::HOLDTYPE_t, HOLD_AMOUNT> holds, const bool
             switch (holds[i])
             {
             case Hold::START_HOLD:
-                  strip.setPixelColor(i, RGB_GREEN);
+                  strip.setPixelColor(i, START_COLOR);
                   break;
             case Hold::RIGHT_HOLD:
+                  strip.setPixelColor(i, RIGHT_COLOR);
+                  break;
             case Hold::LEFT_HOLD:
+                  strip.setPixelColor(i, LEFT_COLOR);
+                  break;
             case Hold::MATCH_HOLD:
+                  strip.setPixelColor(i, MATCH_COLOR);
+                  break;
             case Hold::PROGRESS_HOLD:
+                  strip.setPixelColor(i, PROGRESS_COLOR);
+                  break;
             case Hold::FOOT_HOLD:
+                  strip.setPixelColor(i, FOOT_COLOR);
+                  break;
             case Hold::ADDITIONAL_LED:
-                  strip.setPixelColor(i, RGB_BLUE);
+                  strip.setPixelColor(i, ADDITIONAL_COLOR);
                   break;
             case Hold::END_HOLD:
-                  strip.setPixelColor(i, RGB_RED);
+                  strip.setPixelColor(i, END_COLOR);
                   break;
             case Hold::NO_HOLD:
             default:
-                  strip.setPixelColor(i, RGB_BLACK);
+                  strip.setPixelColor(i, NO_COLOR);
                   break;
             }
       }
